3a8e11df18d0fd1c2855bcdb6e3c39eb94ed0d84
Shubhankar Potnis <shubhankarpotnisofficial@gmail.com>
2026-02-16 21:01:15 +0530

    connection established with Android client using ngrok

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 # python -m venv venv
 # source venv/bin/activate
 # pip install -r requirements.txt
+# 
+
+#import eventlet
+#eventlet.monkey_patch()
 
 from flask import Flask, render_template
 from flask_socketio import SocketIO
@@ -25,6 +29,7 @@ warnings.filterwarnings(
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
 
 # --------------------------------------------------
 # Load ML model ONCE
@@ -76,6 +81,7 @@ def handle_connect():
 # --------------------------------------------------
 @socketio.on('frame')
 def handle_frame(data):
+    print("FRAME RECEIVED")
     try:
         # Decode base64 image
         encoded = data.split(',')[1]
@@ -143,5 +149,5 @@ def process_frame(frame):
 # Main
 # --------------------------------------------------
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    socketio.run(app, host='0.0.0.0', port=5000, debug=True)
 
